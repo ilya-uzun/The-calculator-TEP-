6b4ed981c157f21e2f198f4793a7802c4bab475e
ilya-uzun <ilya.uzun@bk.ru>
2021-07-19 21:28:15 +0500

    update

--- app/src/main/java/com/uzun/the_calculator_tep/GazActivity.kt
+++ app/src/main/java/com/uzun/the_calculator_tep/GazActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import android.widget.EditText
+import com.uzun.the_calculator_tep.constants.GazConstants
 import com.uzun.the_calculator_tep.R
 
 class GazActivity: AppCompatActivity() {
-    public override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var etV: EditText
+    var tvOutputVc: TextView? = null
+
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gaz)
 
@@ -15,6 +21,17 @@ class GazActivity: AppCompatActivity() {
         val tvVc = findViewById<TextView>(R.id.textView_Vc)
         val tvV = findViewById<TextView>(R.id.textView_V)
         val tvK = findViewById<TextView>(R.id.textView_K)
+         tvOutputVc = findViewById<TextView>(R.id.text_view_output_Vс)
+         etV= findViewById<EditText>(R.id.edit_text_enter_V)
 
+         updateOutputVc()
+    }
+
+
+    private fun onEnterV(){
+
+    }
+    private  fun updateOutputVc(){
+        tvOutputVc?.text = "${onEnterV()}"
     }
 }
